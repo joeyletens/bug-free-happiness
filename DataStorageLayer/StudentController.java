91f95f3c60ce4c9351da171edeaa5c11bae6f28c
JoeyLetens <jletens076@gmail.com>
2021-03-23 09:18:47 +0100

    implemented methods from controllerinterface

---
+++ DataStorageLayer/StudentController.java
@@ -0,0 +1,29 @@
+package DataStorageLayer;
+
+public class StudentController implements ControllerInterface {
+
+  @Override
+  public boolean create(Object identifier) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Object read(Object identifier) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean update(Object identifier) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean delete(Object identifier) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
