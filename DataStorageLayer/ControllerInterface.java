2b1f67c721079ce8977fe6c4c013b56d3e59ae75
JoeyLetens <jletens076@gmail.com>
2021-03-23 09:18:14 +0100

    created controllerinterface

---
+++ DataStorageLayer/ControllerInterface.java
@@ -0,0 +1,11 @@
+package DataStorageLayer;
+
+public interface ControllerInterface<T> {
+  public boolean create(T identifier);
+
+  public T read(T identifier);
+
+  public boolean update(T identifier);
+
+  public boolean delete(T identifier);
+}
