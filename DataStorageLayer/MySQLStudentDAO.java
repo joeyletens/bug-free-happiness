8064f7ba5f0d9c19c63bf62a5309afaf9450adea
JoeyLetens <jletens076@gmail.com>
2021-03-24 09:35:23 +0100

    added a studentdao

---
+++ DataStorageLayer/MySQLStudentDAO.java
@@ -0,0 +1,31 @@
+package DataStorageLayer;
+
+import DomainModel.Student;
+
+public class MySQLStudentDAO implements StudentDAOInf {
+
+  @Override
+  public boolean create(Student student) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public String read(Student student) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean update(Student student) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean delete(Student student) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
