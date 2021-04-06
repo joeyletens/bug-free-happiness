71cb2badbd0b6f49378f287fd96eb5d26a3344cf
JoeyLetens <jletens076@gmail.com>
2021-04-06 21:00:00 +0200

    tried starting a method from the main which starts the application

--- Main/main.java
+++ Main/main.java
@@ -1,10 +1,9 @@
 package Main;
 
-import DomainModel.Student;
-import DataStorageLayer.*;
-
 public class Main {
   public static void main(String[] args) {
-    // file
+    // :(
+    Presentation.CreateNewScene.startApplication();
+
   }
 }
