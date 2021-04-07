626fc1a8d2d511005b065cc9bdc184351d695ced
JoeyLetens <jletens076@gmail.com>
2021-04-07 15:29:54 +0200

    Added comments to StudentController

--- ApplicationLogic/StudentController.java
+++ ApplicationLogic/StudentController.java
@@ -1,26 +1,41 @@
 package ApplicationLogic;
 
+import java.sql.SQLException;
+
 import DataStorageLayer.MySQLStudentDAO;
 import DomainModel.Student;
 
-public class StudentController {
+// Studentcontroller creates, reads, deletes or updates a student class
+// the class implements de ControllerInterface 
 
-  public static boolean create(Student student) {
+public class StudentController implements ControllerInterface {
+
+  // Used to create a new student gets called by the GUI
+  public static boolean create(Student student) throws SQLException {
     // TODO Auto-generated method stub
-
+    // if (DataStorageLayer.MySQLStudentDAO.create(student)) {
+    // Presentation.InfoBox("Added successful", student.getFirstName() + " " +
+    // student.getLastName());
+    // return true;
+    // } else {
+    // Presentation.InfoBox("Added failed", "Oh no");
+    // }
     return false;
   }
 
+  // Used to read a new student gets called by the GUI
   public static void read(String email) {
     // TODO Auto-generated method stub
 
   }
 
-  public static boolean update(Student student) {
+  // Used to update a new student gets called by the GUI
+  public static boolean update(String email) {
     // TODO Auto-generated method stub
     return false;
   }
 
+  // Used to delete a new student gets called by the GUI
   public static boolean delete(String email) {
     // TODO Auto-generated method stub
     return false;
