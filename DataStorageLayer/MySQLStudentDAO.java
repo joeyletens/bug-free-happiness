aea082eec0846b9cda2c5d1a57f0f62b83a9ec67
JoeyLetens <jletens076@gmail.com>
2021-04-07 15:31:02 +0200

    Tried something new with prepared statements

--- DataStorageLayer/MySQLStudentDAO.java
+++ DataStorageLayer/MySQLStudentDAO.java
@@ -4,97 +4,80 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import DataStorageLayer.DatabaseConnection.*;
 import DomainModel.Student;
 
 public class MySQLStudentDAO implements StudentDAOInf {
 
   public static ResultSet executeCommand = null;
-  public static PreparedStatement add = null;
+  private static PreparedStatement ps;
+  private static ResultSet rs;
 
-  @Override
   public static boolean create(Student student) throws SQLException {
-
-    // 2. prepare statement
-    String ps = "INSERT INTO Student VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-    // 3. add the variables
-    add.setString(1, student.getEmail());
-    add.setString(2, student.getFirstName());
-    add.setString(3, student.getLastName());
-    add.setString(4, student.getDateOfBirth());
-    add.setString(5, student.getGender());
-    add.setString(6, student.getStreet());
-    add.setString(7, student.getHousenumber());
-    add.setString(8, student.getPostalCode());
-    add.setString(9, student.getCity());
-    add.setString(10, student.getCountry());
-
+    // create query
+    String query = "INSERT INTO Student VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    try {
+      // open connection
+      DatabaseConnection.OpenConnection();
+      ps = DatabaseConnection.conn.prepareStatement(query);
+      // add values to query
+      ps.setString(1, student.getEmail());
+      ps.setString(2, student.getFirstName());
+      ps.setString(3, student.getLastName());
+      ps.setString(4, student.getDateOfBirth());
+      ps.setString(5, student.getGender());
+      ps.setString(6, student.getStreet());
+      ps.setString(7, student.getHousenumber());
+      ps.setString(8, student.getPostalCode());
+      ps.setString(9, student.getCity());
+      ps.setString(10, student.getCountry());
+      // execute and close prepared statement
+      ps.executeUpdate();
+      ps.close();
+      // finally close connection
+      DatabaseConnection.CloseConnection();
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+      return false;
+    }
     return true;
-
   }
 
-  @Override
-  public static String read(Student student) {
+  public static ResultSet read(String email) throws SQLException {
 
     String query = "SELECT * FROM Student WHERE email = '?'";
+    try {
+      ps.setString(1, email);
+      rs = ps.executeQuery();
+      ps.close();
 
-    add.setString(1, student.getEmail());
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+      return null;
+    }
 
-    return null;
-
+    return rs;
   }
 
-  @Override
   public static boolean update(Student student) {
-    try {
 
-      // 1. Get a connection to database
-      DatabaseConnection.OpenConnection();
+    // 2. prepare statement
+    String ps = "UPDATE Student SET email = '?'";
 
-      // 2. prepare statement
-      String ps = "UPDATE Student SET email = '?'";
-
-      // 3. add the variables
-      add.setString(1, student.getEmail());
-
-      // 4. execute SQL query
-      executeCommand = add.executeQuery();
-
-    } catch (SQLException e) {
-      e.printStackTrace();
-      return false;
-    }
-
-    DatabaseConnection.CloseConnection();
+    // 3. add the variables
+    // add.setString(1, student.getEmail());
 
     return true;
   }
 
-  @Override
   public static boolean delete(Student student) {
-    try {
 
-      // 1. Get a connection to database
-      DatabaseConnection.OpenConnection();
+    // 2. prepare statement
+    String ps = "DELETE FROM Student WHERE email = '?'";
 
-      // 2. prepare statement
-      String ps = "DELETE FROM Student WHERE email = '?'";
-
-      // 3. add the variables
-      add.setString(1, student.getEmail());
-
-      // 4. execute SQL query
-      executeCommand = add.executeQuery();
-
-    } catch (SQLException e) {
-      e.printStackTrace();
-      return false;
-    }
-
-    DatabaseConnection.CloseConnection();
+    // // 3. add the variables
+    // add.setString(1, student.getEmail());
 
     return true;
-  }
 
+  }
 }
