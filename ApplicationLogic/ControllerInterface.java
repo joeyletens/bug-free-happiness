780816aa4b92da22442e8d873c3f8e169fcccb53
JoeyLetens <jletens076@gmail.com>
2021-04-07 15:29:30 +0200

    Added comments to ControllerInterface

--- ApplicationLogic/ControllerInterface.java
+++ ApplicationLogic/ControllerInterface.java
@@ -1,5 +1,7 @@
 package ApplicationLogic;
 
+// interface for the StudentController and Registration controller
+// Gives the classes create, read, update and delete (CRUD)
 public interface ControllerInterface {
   public static boolean create() {
     return false;
