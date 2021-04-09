badb7f50d17755108d8c596acd0a129ffa8aead5
JoeyLetens <jletens076@gmail.com>
2021-04-09 09:38:29 +0200

    Added getters and setters

--- DomainModel/ContentItem.java
+++ DomainModel/ContentItem.java
@@ -10,11 +10,20 @@ public class ContentItem {
   private int duration;
   private int progress;
 
-  public ContentItem(int contentId, Date publicationDate, String status, String description) {
+  public ContentItem(int contentId, Date publicationDate, String status, String description, int duration) {
     this.contentId = contentId;
     this.publicationDate = publicationDate;
     this.status = status;
     this.description = description;
+    this.duration = duration;
+  }
+
+  public int getProgress() {
+    return progress;
+  }
+
+  public void setProgress(int progress) {
+    this.progress = progress;
   }
 
 }
