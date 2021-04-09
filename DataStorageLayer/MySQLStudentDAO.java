0b4abbcdbb4a598b4afc06461971a0fa3b80934d
JoeyLetens <jletens076@gmail.com>
2021-04-09 09:38:13 +0200

    Moved openConnection

--- DataStorageLayer/MySQLStudentDAO.java
+++ DataStorageLayer/MySQLStudentDAO.java
@@ -18,10 +18,11 @@ public class MySQLStudentDAO implements StudentDAOInf {
   // This creates a new student with a prepared statement
   public static boolean create(Student student) throws SQLException {
     // create query
-    String query = "INSERT INTO Student VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    DatabaseConnection.OpenConnection();
+    String query = "INSERT INTO StudentVerticalSlice VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     try {
       // open connection
-      DatabaseConnection.OpenConnection();
+
       ps = DatabaseConnection.conn.prepareStatement(query);
       // add values to query
       ps.setString(1, student.getEmail());
@@ -49,7 +50,7 @@ public class MySQLStudentDAO implements StudentDAOInf {
   // Method to send a request to the database to read data with a specifict email
   public static ResultSet read(String email) throws SQLException {
 
-    String query = "SELECT * FROM Student WHERE email = '?'";
+    String query = "SELECT * FROM StudentVerticalSlice WHERE email = '?'";
     try {
       ps.setString(1, email);
       rs = ps.executeQuery();
