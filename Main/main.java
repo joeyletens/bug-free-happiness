b74e96569f059b4985f919c7644d0faa97a456a8
JoeyLetens <jletens076@gmail.com>
2021-04-09 22:18:13 +0200

    Tested some methods in main

--- Main/main.java
+++ Main/main.java
@@ -1,10 +1,24 @@
 package Main;
 
+import java.sql.SQLException;
+
+import DataStorageLayer.DatabaseConnection;
+import DataStorageLayer.MySQLStudentDAO;
+import DomainModel.Student;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SQLException {
+
+    Student student = new Student("email", "joey", "letens", "15/04/2021", "male", "4848kp", "sesam", "68", "breda",
+        "netherlands");
+
+    System.out.println("LALALALALAL");
+
+    System.out.println(student);
+
+    // DataStorageLayer.MySQLStudentDAO.create(student);
 
     launch(Presentation.CreateNewScene.class);
 
