62ae1fd3ddc753739f16ea658696a4f13b5df898
JoeyLetens <jletens076@gmail.com>
2021-04-12 08:12:49 +0200

    Cleaned up the main

--- Main/main.java
+++ Main/main.java
@@ -1,32 +1,22 @@
 package Main;
 
-import java.sql.SQLException;
-
-import DataStorageLayer.DatabaseConnection;
-import DataStorageLayer.MySQLStudentDAO;
-import DomainModel.Student;
+import Presentation.CreateNewScene;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-  public static void main(String[] args) throws SQLException {
+  public static void main(String[] args) {
 
-    Student student = new Student("email", "joey", "letens", "15/04/2021", "male", "4848kp", "sesam", "68", "breda",
-        "netherlands");
-
-    System.out.println("LALALALALAL");
-
-    System.out.println(student);
-
-    // DataStorageLayer.MySQLStudentDAO.create(student);
-
-    launch(Presentation.CreateNewScene.class);
+    launch(Main.class);
 
   }
 
   @Override
   public void start(Stage arg0) throws Exception {
     // TODO Auto-generated method stub
+
+    CreateNewScene createNewScene = new CreateNewScene();
+    createNewScene.createWindow();
 
   }
 }
