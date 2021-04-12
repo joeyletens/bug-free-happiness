c521a4604d80c64b0430b47c0c0d7beb1935f8a8
JoeyLetens <jletens076@gmail.com>
2021-04-12 23:19:16 +0200

    Created method to return webcastPage gridpane

--- Presentation/Course/WebcastPage.java
+++ Presentation/Course/WebcastPage.java
@@ -1,14 +1,69 @@
 package Presentation.Course;
 
-import Presentation.PresentationInf;
-import javafx.scene.layout.GridPane;
+import Presentation.CreateNewScene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.geometry.*;
 
-public class WebcastPage implements PresentationInf {
+public class WebcastPage {
+  // Create different variables
+  private TextField email, title, contentId, progress;
+  private Button backToCoursePage, createCourseBtn;
+  private int buttonWidth = 30;
+  private int buttonHeight = 10;
 
-  @Override
   public GridPane GUI() {
-    // TODO Auto-generated method stub
-    return null;
-  }
+    GridPane form = new GridPane();
+    form.setHgap(25);
+    form.setVgap(10);
+    form.setPadding(new Insets(15, 25, 15, 25));
 
+    // Title
+    form.add(new Label("VIEW: enter student and module details below"), 0, 0, 3, 1);
+
+    // Add student email box
+    form.add(new Label("Email:"), 0, 1);
+    email = new TextField();
+    form.add(email, 0, 2, 3, 1);
+
+    // Add course title box
+    form.add(new Label("Title:"), 0, 3);
+    title = new TextField();
+    form.add(title, 0, 4, 3, 1);
+
+    // Add course contentId box
+    form.add(new Label("ContentId:"), 0, 5);
+    contentId = new TextField();
+    form.add(contentId, 0, 6, 3, 1);
+
+    // Add course progress made my student box
+    form.add(new Label("Progress:"), 0, 7);
+    progress = new TextField();
+    form.add(progress, 0, 8, 3, 1);
+
+    // Go back to Course page
+    backToCoursePage = new Button("Back to Course page");
+    CoursePage coursePage = new CoursePage();
+    backToCoursePage.setOnAction(e -> CreateNewScene.changeScene((coursePage.GUI())));
+    backToCoursePage.setMinSize(buttonWidth, buttonHeight);
+    form.add(backToCoursePage, 0, 9);
+
+    // Creates a new Course when pressed on the button
+    createCourseBtn = new Button("Make progress");
+    createCourseBtn.setMinSize(buttonWidth, buttonHeight);
+    createCourseBtn.setOnAction(e -> {
+
+      // CourseController courseController = new CourseController();
+      // try {
+      // courseController.read(email.getText());
+      // } catch (SQLException e1) {
+      // // TODO Auto-generated catch block
+      // e1.printStackTrace();
+      // }
+
+    });
+    form.add(createCourseBtn, 2, 9);
+    return form;
+
+  }
 }
