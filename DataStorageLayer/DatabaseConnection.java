e7c3f38e260b2587facc11af21199b5207fb3d1e
JoeyLetens <jletens076@gmail.com>
2021-04-12 08:10:41 +0200

    Created a add student method

--- DataStorageLayer/DatabaseConnection.java
+++ DataStorageLayer/DatabaseConnection.java
@@ -2,87 +2,107 @@ package DataStorageLayer;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
+
+import DomainModel.Student;
+import Presentation.InfoBox;
 
 // this class opens and closes database connection
 // executes all 4 CRUD statements. 
 
 public class DatabaseConnection {
 
-  public Connection conn = null;
-  private Statement stmt;
   private ResultSet rs;
   private final String DB_USERNAME = "sa";
   private final String DB_PASSWORD = "QuattroIsLeuk";
   private final String DB_URL = "jdbc:sqlserver://localhost\\MSSQLSERVER;databaseName=CodecademyStudent";
+  private final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+  public Connection conn = null;
 
-  // open a connection with the database
-  public boolean openConnection() {
+  private final String SQL_INSERT = "INSERT INTO StudentVerticalSlice VALUES (?, ?, ? ,? , ?, ?, ?, ?, ?, ?);";
+  private final String SQL_SELECT = "SELECT * FROM StudentVerticalSlice WHERE email = ?;";
+  private PreparedStatement ps;
+
+  // // Executes insert statement
+  public boolean ExecuteInsertStatement(Student student) throws SQLException {
 
     try {
-      // database URL
-      // Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
+      // import and get connection
+      Class.forName(JDBC_DRIVER);
       conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+      ps = conn.prepareStatement(SQL_INSERT);
+
+      // Added paramter values to prepared statement
+      ps.setString(1, student.getEmail());
+      ps.setString(2, student.getFirstName());
+      ps.setString(3, student.getLastName());
+      ps.setString(4, student.getDateOfBirth());
+      ps.setString(5, student.getGender());
+      ps.setString(6, student.getStreet());
+      ps.setString(7, student.getHousenumber());
+      ps.setString(8, student.getPostalCode());
+      ps.setString(9, student.getCity());
+      ps.setString(10, student.getCountry());
+
+      // Execute the prepared statement
+      ps.executeUpdate();
+
+      // Display the students first and last name on a pop-up screen
+      InfoBox.Display("Added student", student.getFirstName() + " " + student.getLastName());
 
       return true;
 
     } catch (Exception e) {
       e.printStackTrace();
       return false;
+    } finally {
+      if (conn != null) {
+        conn.close();
+      }
+      if (ps != null) {
+        ps.close();
+      }
     }
   }
 
-  // Closes connection with database
-  // public final boolean CloseConnection() throws SQLException {
-  // try {
-  // conn.close();
-  // return true;
-  // } catch (Exception e) {
-  // e.printStackTrace();
-  // return false;
-  // }
-  // }
-
-  // // Executes insert statement
-  // public final boolean ExecuteInsertStatement(String query, PreparedStatement
-  // ps) throws SQLException {
-  // OpenConnection();
-
-  // stmt = conn.createStatement();
-
-  // rs = stmt.executeQuery(query);
-
-  // CloseConnection();
-
-  // return true;
-  // }
-
   // // excute the prepared statement
-  // public final ResultSet ExecuteSelectStatement(String query,
-  // PreparedStatement ps) throws SQLException {
+  public ResultSet ExecuteSelectStatement(String email) throws SQLException {
 
-  // OpenConnection();
+    try {
 
-  // stmt = conn.createStatement();
+      Class.forName(JDBC_DRIVER);
 
-  // rs = stmt.executeQuery(query);
+      conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 
-  // CloseConnection();
+      // Create prepatedStatement
+      ps = conn.prepareStatement(SQL_SELECT);
 
-  // return rs;
+      ps.setString(1, email);
 
-  // }
+      rs = ps.executeQuery();
 
-  // public boolean ExecuteDeleteStatement(Student student) throws SQLException {
+      if (rs.next()) {
+        return rs;
+      } else {
+        return null;
+      }
 
-  // return true;
-  // }
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
 
-  // public boolean ExecuteUpdateStatement(Student student) throws SQLException {
+  public boolean ExecuteDeleteStatement() throws SQLException {
 
-  // return true;
-  // }
+    return true;
+  }
+
+  public boolean ExecuteUpdateStatement() throws SQLException {
+
+    return true;
+  }
 
 }
