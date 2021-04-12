786c08c0c3ec21d8c2a2ecf279df1043acd16e87
JoeyLetens <jletens076@gmail.com>
2021-04-12 08:10:20 +0200

    Added functionality to create student

--- ApplicationLogic/StudentController.java
+++ ApplicationLogic/StudentController.java
@@ -1,14 +1,17 @@
 package ApplicationLogic;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import DataStorageLayer.MySQLStudentDAO;
-import DomainModel.Student;
+import DataStorageLayer.DatabaseConnection;
+import Presentation.InfoBox;
 
 // Studentcontroller creates, reads, deletes or updates a student class
 // the class implements de ControllerInterface 
 
 public class StudentController implements ControllerInterface {
+  DatabaseConnection dao = new DatabaseConnection();
+  StringBuilder message = new StringBuilder();
 
   @Override
   public boolean create() {
@@ -17,9 +20,18 @@ public class StudentController implements ControllerInterface {
   }
 
   @Override
-  public String read() {
-    // TODO Auto-generated method stub
-    return null;
+  public void read(String email) throws SQLException {
+    ResultSet rs = dao.ExecuteSelectStatement(email);
+    if (rs == null) {
+      InfoBox.Display("Student not found", "Student with the email: \n" + email + "\nwas not found");
+    } else {
+      message.append("Email: " + rs.getString(1) + "\n");
+      message.append("Name: " + rs.getString(2) + " " + rs.getString(3) + "\n");
+      message.append("Date of birth: " + rs.getString(4) + "\n");
+      message.append("Gender : " + rs.getString(5) + "\n");
+
+      InfoBox.Display("Student was found", message.toString());
+    }
   }
 
   @Override
