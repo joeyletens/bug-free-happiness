c13a8a57bf71dcd87fb018049e849d4556f1edd3
JoeyLetens <jletens076@gmail.com>
2021-04-12 08:09:48 +0200

    Changed controllerinterface parameters

--- ApplicationLogic/ControllerInterface.java
+++ ApplicationLogic/ControllerInterface.java
@@ -1,13 +1,15 @@
 package ApplicationLogic;
 
+import java.sql.SQLException;
+
 // interface for the StudentController and Registration controller
 // Gives the classes create, read, update and delete (CRUD)
 public interface ControllerInterface {
   public boolean create();
 
-  public String read();
+  public void read(String email) throws SQLException;
 
   public boolean update();
 
-  public boolean delete();
+  public boolean delete(String email);
 }
