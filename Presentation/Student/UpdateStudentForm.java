7bd6d37ce3465c29c45318eea86818e392b7c344
JoeyLetens <jletens076@gmail.com>
2021-04-12 09:47:41 +0200

    Added button and newEmail field

--- Presentation/Student/UpdateStudentForm.java
+++ Presentation/Student/UpdateStudentForm.java
@@ -2,12 +2,16 @@ package Presentation.Student;
 
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+
+import java.sql.SQLException;
+
+import ApplicationLogic.StudentController;
 import Presentation.CreateNewScene;
 import javafx.geometry.*;
 
 public class UpdateStudentForm {
 
-  static TextField email;
+  static TextField email, newEmail;
   static Button cancel, add;
 
   // UPDATE STUDENT DATA
@@ -24,18 +28,27 @@ public class UpdateStudentForm {
     email = new TextField();
     form.add(email, 0, 2, 3, 1);
 
+    // Add newEmail textfield
+    form.add(new Label("Email:"), 0, 3, 3, 1);
+    newEmail = new TextField();
+    form.add(newEmail, 0, 4, 3, 1);
+
     // Create back to student page button
     Button backToStudentPage = new Button("Back to student page");
     backToStudentPage.setOnAction(e -> CreateNewScene.changeScene(StudentPage.GUI()));
-    form.add(backToStudentPage, 0, 3);
+    form.add(backToStudentPage, 0, 5);
 
     // Creates a new student when pressed on the button
-
     Button createStudentBtn = new Button("Get data");
     createStudentBtn.setOnAction(e -> {
-
+      StudentController studentController = new StudentController();
+      try {
+        studentController.update(email.getText(), newEmail.getText());
+      } catch (SQLException e1) {
+        e1.printStackTrace();
+      }
     });
-    form.add(createStudentBtn, 2, 3);
+    form.add(createStudentBtn, 2, 5);
 
     return form;
   }
