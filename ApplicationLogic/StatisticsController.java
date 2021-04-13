31f4e85d958679c85109a18317eea507c4eb8e81
JoeyLetens <jletens076@gmail.com>
2021-04-13 15:27:00 +0200

    Added method functionality

--- ApplicationLogic/StatisticsController.java
+++ ApplicationLogic/StatisticsController.java
@@ -1,8 +1,44 @@
 package ApplicationLogic;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import DataStorageLayer.SQLStatisticsDAO;
+import Presentation.InfoBox;
+
 public class StatisticsController {
+  SQLStatisticsDAO dao = new SQLStatisticsDAO();
+  StringBuilder selectList;
+  ResultSet rs;
+
   public void averageProgressPerModule(String courseName) {
     // for a selected course give the average progress per module
+    try {
+      rs = dao.ExecuteAverageProgress(courseName);
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    if (rs == null) {
+      // If resultset is empty display not found
+      InfoBox.Display("", "");
+    } else {
+      // else read through the whole list
+      selectList = new StringBuilder();
+      try {
+        while (rs.next()) {
+          selectList.append("===============================");
+          selectList.append("Date: " + rs.getString(1) + "\n");
+          selectList.append("Email: " + rs.getString(2) + "\n");
+          selectList.append("Student: " + rs.getString(3) + "\n");
+          selectList.append("===============================");
+        }
+      } catch (SQLException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+      InfoBox.Display("Registrations found", selectList.toString());
+    }
   }
 
   public void studentProgressPerModule(String courseName, String email) {
