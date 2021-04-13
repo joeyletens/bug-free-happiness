fc305b5e4eb6959fb65860447976b872ffc5b924
JoeyLetens <jletens076@gmail.com>
2021-04-13 10:42:20 +0200

    Created file and methods for statistics controller

--- ApplicationLogic/StatisticsController.java
+++ ApplicationLogic/StatisticsController.java
@@ -1,5 +1,19 @@
 package ApplicationLogic;
 
 public class StatisticsController {
+  public void averageProgressPerModule() {
+    // for a selected course give the average progress per module
+  }
 
+  public void studentProgressPerModule() {
+    // for a selected course and student give the progress per module in percentage
+  }
+
+  public void topThreeMostViewedWebcasts() {
+    // give the three most viewed webcasts
+  }
+
+  public void studentsWhoPassedACourse() {
+    // amount of students who passed a certain course
+  }
 }
