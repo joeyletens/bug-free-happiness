422d75e45d3f95d6b128d08ae876368d86b2b3f7
JoeyLetens <jletens076@gmail.com>
2021-04-13 08:48:59 +0200

    Created course controller and added methods

--- ApplicationLogic/CourseController.java
+++ ApplicationLogic/CourseController.java
@@ -1,5 +1,28 @@
 package ApplicationLogic;
 
+import DomainModel.Viewed;
+
 public class CourseController {
+  public void getModuleProgress(Viewed viewed) {
+    // This method return a students progress in a certain module and display it
+  }
+
+  public void getWebcastProgress(Viewed viewed) {
+    // This method returns the progress made by a student and displays it
+  }
+
+  public void makeProgressInModule(int progress, Viewed viewed) {
+    // lets a student make progress in a module
+  }
+
+  public void makeProgressInWebcast(int progress, Viewed viewed) {
+    // lets a student make progress in a webcast
+  }
+
+  public boolean checkIfStudentIsRegisteredToCourse(Viewed viewed) {
+    // This method returns a boolean value to check if the student is registrered to
+    // a course
+    return false;
+  }
 
 }
