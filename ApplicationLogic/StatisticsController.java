10ac2df8ede35b76be25fb9932a8daec0d43b9ec
JoeyLetens <jletens076@gmail.com>
2021-04-13 10:50:27 +0200

    Added parameters to controller

--- ApplicationLogic/StatisticsController.java
+++ ApplicationLogic/StatisticsController.java
@@ -1,11 +1,11 @@
 package ApplicationLogic;
 
 public class StatisticsController {
-  public void averageProgressPerModule() {
+  public void averageProgressPerModule(String courseName) {
     // for a selected course give the average progress per module
   }
 
-  public void studentProgressPerModule() {
+  public void studentProgressPerModule(String courseName, String email) {
     // for a selected course and student give the progress per module in percentage
   }
 
@@ -13,7 +13,7 @@ public class StatisticsController {
     // give the three most viewed webcasts
   }
 
-  public void studentsWhoPassedACourse() {
+  public void studentsWhoPassedACourse(String courseName) {
     // amount of students who passed a certain course
   }
 }
