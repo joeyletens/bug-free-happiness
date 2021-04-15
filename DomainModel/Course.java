e82e0d6a1ac9b50653c8a3a4264b1d749c8caa9d
JoeyLetens <jletens076@gmail.com>
2021-04-15 11:08:24 +0200

    Removed followNumber from courses and modules

--- DomainModel/Course.java
+++ DomainModel/Course.java
@@ -3,15 +3,13 @@ package DomainModel;
 import java.util.ArrayList;
 
 public class Course {
-  private int idNumber;
   private String courseName;
   private String subject;
   private String introductionText;
   private Level courseLevel;
   private ArrayList<Course> relatedCourses;
 
-  public Course(int idNumber, String courseName, String subject, String introductionText, Level courseLevel) {
-    this.idNumber = idNumber;
+  public Course(String courseName, String subject, String introductionText, Level courseLevel) {
     this.courseName = courseName;
     this.subject = subject;
     this.introductionText = introductionText;
@@ -27,14 +25,6 @@ public class Course {
     if (!relatedCourses.contains(course)) {
       relatedCourses.add(course);
     }
-  }
-
-  public int getIdNumber() {
-    return idNumber;
-  }
-
-  public void setIdNumber(int idNumber) {
-    this.idNumber = idNumber;
   }
 
   public String getCourseName() {
--- DomainModel/Modules.java
+++ DomainModel/Modules.java
@@ -6,19 +6,24 @@ public class Modules extends ContentItem {
   private String title;
   private String version;
   private String contactPersonEmail;
-  private int courseNumber;
+  private int followNumber;
+  private Course course;
 
   public Modules(int contentId, Date publicationDate, String status, String description, String title, String version,
-      String contactPersonEmail, int courseNumber, int progress) {
+      String contactPersonEmail, int followNumber, int progress) {
     super(contentId, publicationDate, status, description, progress);
     this.title = title;
     this.version = version;
     this.contactPersonEmail = contactPersonEmail;
-    this.courseNumber = courseNumber;
+    this.followNumber = followNumber;
   }
 
   public String getTitle() {
     return title;
+  }
+
+  public Course returnCourse() {
+    return course;
   }
 
   public void setTitle(String title) {
@@ -41,12 +46,12 @@ public class Modules extends ContentItem {
     this.contactPersonEmail = contactPersonEmail;
   }
 
-  public int getCourseNumber() {
-    return courseNumber;
+  public int getfollowNumber() {
+    return followNumber;
   }
 
-  public void setCourseNumber(int courseNumber) {
-    this.courseNumber = courseNumber;
+  public void setfollowNumber(int followNumber) {
+    this.followNumber = followNumber;
   }
 
 }
